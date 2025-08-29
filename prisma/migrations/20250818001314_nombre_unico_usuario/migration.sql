848fd5323c381143650b7e123fd3b11c74f850c4
DAVID SABOGAL <sabogaldavid26@gmail.com>
2025-08-28 20:37:39 -0500

    Delete prisma/migrations directory

--- prisma/migrations/20250818001314_nombre_unico_usuario/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nombre]` on the table `Usuario` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Usuario_nombre_key` ON `Usuario`(`nombre`);
